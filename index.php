94567796c055041dbf5f49aa0e4adf03f51c1ecd
K.B. Carte <kbcarte@gmail.com>
2018-03-25 18:37:55 -0400

    refactored the code, added doc strings, ready for testing/public download

--- index.php
+++ index.php
@@ -6,16 +6,46 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 	<?php include "ziplib.php"; ?>
-	<title>ZipNews</title>
+	<title>News API</title>
 </head>
 <body>
 	<div class="container">
 		<ul class="article_list">
 			<?php
+			// Use getenv() instead of $_ENV, the later needs to be set in
+			// php.ini to work and is recommended not to be used in production
+			$key = getenv()["NEWS_API_KEY"];
+			$newsObj = new NewsApiClient($key);
+			$args = array('language'=>'en',
+						'pageSize'=>'30',
+						'sources'=>array("ign", "hacker-news", "techradar", "wired", "the-next-web", "techcrunch"));
+			
+			$obj = $newsObj->get_top_headlines($args);
+			$article_list = array();
 
-			$newsObj = new NewsApiClient('DaminIt_Ineed_EnvVars_');
-			$n = $newsObj->get_sources('');
-			print_r($n);
+			// if any of the data has null or no values for the things we want to post
+			//   skip them. Not every article will have everything we need
+			for ($i=0; $i < count($obj["articles"]); $i++) {
+				if ( !is_null($obj["articles"][$i]['url']) and 
+					!is_null($obj['articles'][$i]['title']) and 
+					!is_null($obj['articles'][$i]['urlToImage']) and 
+					!is_null($obj['articles'][$i]['description']) and
+					!is_null($obj['articles'][$i]['author']) ) {
+						// checking for duplicates
+						if (!in_array($obj['articles'][$i]['title'], $article_list)) {
+							// push title to array to check on next loop
+							array_push($article_list, $obj['articles'][$i]['title']);
+
+							// ugly echo html, at least we give the elements classes for css
+							echo "<li class=\"article_entry\"><a target=\"_blank\" href=\"". $obj["articles"][$i]['url'] . "\">";
+							echo "<h2 class=\"article_title\">" . $obj["articles"][$i]['title'] . "</h2>";
+							echo "<h3 class=\"article_publisher\"> From: " . $obj['articles'][$i]['source']['name'] . "</h3>";
+							echo "<img class=\"article_image\" src=\"". $obj['articles'][$i]['urlToImage'] . "\">";
+							echo "<p class=\"article_description\">" . $obj['articles'][$i]['description'] . "</p>";
+							echo "</li></a>";
+						}
+					}
+			}
 			
 			?>
 		</ul>
--- ziplib.php
+++ ziplib.php
@@ -1,11 +1,12 @@
 <?php
     /*
-    * Direct port from the Python lib found here
+    * Doc Strings are pulled from the python lib found here:
     * -> https://github.com/mattlisiv/newsapi-python/blob/master/newsapi/newsapi_client.py
     *
-    *
-    *
-    *
+    * Author: K.B. Carte
+    * Author URL: https://kbcarte.com
+    * Date: March 2018
+    * License: MIT
     *
     */
     class NewsApiClient {
@@ -14,22 +15,49 @@
         public $api_url;
 
         public function __construct($api_key, $api_url='https://newsapi.org/v2/'){
+            /* 
+            * api key should not be in these files, either have a seporate file
+            * containing it, or do like I did and set it as an environment var
+            */
             $this->api_key = 'apiKey=' . $api_key;
             $this->api_url = $api_url;
         }
 
-        public function get_top_headlines($q=null, $sources=null, $language=null, $country=null, $category=null, $page_size=null, $page=null){
-            $payload = array();
-            $payload['q'] = $q;
-            $payload['sources'] = implode(',', $sources);
-            $payload['language'] = $language;
-            $payload['country'] = $country;
-            $payload['category'] = $category;
-            $payload['pageSize'] = $page_size;
-            $payload['page'] = $page;
+        public function get_top_headlines($args=array()){
+            /*
+            * Returns live top and breaking headlines for a country, specific category in a country,
+            * single source, or multiple sources..
+            *
+            * Optional parameters:
+            *   (str) q - return headlines w/ specified coin! Valid values are:
+            *       'bitcoin', 'etheremum', 'ripple', 'bitcoin cash', etc.
+            *                
+            *    (array) sources - return headlines of news sources! some Valid values are:
+            *       'bbc-news', 'the-verge', 'abc-news', 'crypto-coins-news', etc...
+            *       Full list found here: https://newsapi.org/sources
+			*				
+			*	(str) language - The 2-letter ISO-639-1 code of the language you want to get headlines for. Valid values are:
+			*	    'ar','de','en','es','fr','he','it','nl','no','pt','ru','se','ud','zh'
+            *                
+            *    (str) country - The 2-letter ISO 3166-1 code of the country you want to get headlines! Valid values are:
+            *       'ae','ar','at','au','be','bg','br','ca','ch','cn','co','cu','cz','de','eg','fr','gb','gr',
+            *       'hk','hu','id','ie','il','in','it','jp','kr','lt','lv','ma','mx','my','ng','nl','no','nz',
+            *       'ph','pl','pt','ro','rs','ru','sa','se','sg','si','sk','th','tr','tw','ua','us'
+			*					
+			*	(str) category - The category you want to get headlines for! Valid values are:
+			*       'business','entertainment','general','health','science','sports','technology'
+			*					
+			*	(int) pageSize - The number of results to return per page (request). 20 is the default, 100 is the maximum. 
+			*					
+		    *	(int) page - Use this to page through the results if the total results found is greater than the page
+            */
+
+            if (!empty($args['sources'])){
+                $args['sources'] = implode(',', $args['sources']);
+            }
 
             $_url = $this->api_url . 'top-headlines?' . $this->api_key;
-            foreach ($payload as $key=>$item){
+            foreach ($args as $key=>$item){
                 if (!is_null($item) and !empty($item)){
                     $_url .= '&' . $key . '=' .$item;
                 }
@@ -41,21 +69,42 @@
         }
 
 
-        public function get_everything($q=null, $sources=null, $language=null, $from_parameter=null, $to=null, $domains=null, $sort_by=null, $page=null, $page_size=null){
+        public function get_everything($args=array()){
+            /*     
+            * Search through millions of articles from over 5,000 large and small news sources and blogs.
+            * Optional parameters:
+            *   (str) q - return headlines w/ specified coin! Valid values are:
+            *       'bitcoin', 'etheremum', 'ripple', 'bitcoin cash', etc.
+            *                    
+            *   (array) sources - return headlines of news sources! some Valid values are:
+            *       'bbc-news', 'the-verge', 'abc-news', 'crypto-coins-news', etc...
+            *       Full list found here: https://newsapi.org/sources
+            *					
+            *   (str) domains - A comma-seperated string of domains to restrict the search to.
+            *       (eg bbc.co.uk, techcrunch.com, engadget.com) 
+            * 
+            *   (str) from - A date and optional time for the oldest article allowed.
+            *       (e.g. 2018-03-05 or 2018-03-05T03:46:15)
+            *		
+            *   (str) to - A date and optional time for the newest article allowed.
+            *					
+            *   (str) language - The 2-letter ISO-639-1 code of the language you want to get headlines for:
+            * 	    'ar','de','en','es','fr','he','it','nl','no','pt','ru','se','ud','zh'
+            *						
+            *   (str) sortBy - The order to sort the articles in. Valid values are:
+            *       'relevancy','popularity','publishedAt', 'relevancy'
+            *						
+            *   (int or str) pageSize - The number of results to return per page (request). 20 is the default, 100 is the maximum.	
+            *	
+            *   (int or str) page - Use this to page through the results if the total results found is greater than the page size.
+            */
 
-            $payload = array();
-            $payload['q'] = $q;
-            $payload['sources'] = implode(',', $sources);
-            $payload['domains'] = $domains;
-            $payload['from'] = $from_parameter;
-            $payload['to'] = $to;
-            $payload['language'] = $language;
-            $payload['sortBy'] = $sort_by;
-            $payload['page'] = $page;
-            $payload['pageSize'] = $page_size;
+            if (!empty($args['sources'])){
+                $args['sources'] = implode(',', $args['sources']);
+            }
 
             $_url = $this->api_url . 'everything?' . $this->api_key;
-            foreach ($payload as $key=>$item){
+            foreach ($args as $key=>$item){
                 if (!is_null($item) and !empty($item)){
                     $_url .= '&' . $key . '=' .$item;
                 }
@@ -64,18 +113,27 @@
             $json = file_get_contents($_url);
             $obj = json_decode($json, true);
             return $obj;
+            
         }
 
-
-        public function get_sources($category=null, $language=null, $country=null){
-
-            $payload = array();
-            $payload['category'] = $category;
-            $payload['language'] = $language;
-            $payload['country'] = $country;
+        public function get_sources($args=array()){
+            /*
+            * Returns the subset of news publishers that top headlines...
+            * Optional parameters:
+            *   (str) language - The 2-letter ISO-639-1 code of the language you want to get headlines for. Valid values are:
+			*	    'ar','de','en','es','fr','he','it','nl','no','pt','ru','se','ud','zh'
+            *                
+            *    (str) country - The 2-letter ISO 3166-1 code of the country you want to get headlines! Valid values are:
+            *       'ae','ar','at','au','be','bg','br','ca','ch','cn','co','cu','cz','de','eg','fr','gb','gr',
+            *       'hk','hu','id','ie','il','in','it','jp','kr','lt','lv','ma','mx','my','ng','nl','no','nz',
+            *       'ph', 'pl','pt','ro','rs','ru','sa','se','sg','si','sk','th','tr','tw','ua','us'
+			*					
+			*	(str) category - The category you want to get headlines for! Valid values are:
+			*	    'business','entertainment','general','health','science','sports','technology'
+            */
 
             $_url = $this->api_url . 'sources?' . $this->api_key;
-            foreach ($payload as $key=>$item){
+            foreach ($args as $key=>$item){
                 if (!is_null($item) and !empty($item)){
                     $_url .= '&' . $key . '=' .$item;
                 }
